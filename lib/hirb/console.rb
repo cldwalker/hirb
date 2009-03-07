9df94f6f3b284fee2abb6052ea37fc5712ed30b6
Gabriel Horner <gabriel.horner@gmail.com>
2009-03-06 19:17:46 -0500

    removing include support for Hirb::Console

--- lib/hirb/console.rb
+++ lib/hirb/console.rb
@@ -1,9 +1,5 @@
 module Hirb
   module Console
-    def self.included(base)
-      base.extend self
-    end
-    
     def table(output, options={})
       Hirb::View.console_output_value(output, options.merge(:class=>"Hirb::Helpers::AutoTable"))
     end
