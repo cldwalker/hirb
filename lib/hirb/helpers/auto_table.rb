4462347a8e58ec1f27302023b9756d20425b1bc4
Gabriel Horner <gabriel.horner@gmail.com>
2009-05-26 19:33:55 -0400

    more useful auto_table

--- lib/hirb/helpers/auto_table.rb
+++ lib/hirb/helpers/auto_table.rb
@@ -4,7 +4,7 @@ class Hirb::Helpers::AutoTable
   def self.render(output, options={})
     klass = if ((output.is_a?(Array) && output[0].is_a?(ActiveRecord::Base)) or output.is_a?(ActiveRecord::Base) rescue false)
       Hirb::Helpers::ActiveRecordTable
-    elsif options[:fields]
+    elsif (output.is_a?(Array) && !(output[0].is_a?(Hash) || output[0].is_a?(Array)))
       Hirb::Helpers::ObjectTable
     else
       Hirb::Helpers::Table
