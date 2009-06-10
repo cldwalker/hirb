55c3673f9e7215e88343616cbe75ac71463de256
Gabriel Horner <gabriel.horner@gmail.com>
2009-06-10 13:56:21 -0400

    ask option + blocks for menu

--- lib/hirb/helpers/menu.rb
+++ lib/hirb/helpers/menu.rb
@@ -1,7 +1,11 @@
 class Hirb::Helpers::Menu
   def self.render(output, options={})
-    options = {:helper_class=>Hirb::Helpers::Table, :prompt=>"Choose #{options[:choose] || ""}: "}.merge(options)
+    options = {:helper_class=>Hirb::Helpers::Table, :prompt=>"Choose #{options[:choose] || ""}: ", :ask=>true}.merge(options)
     options[:helper_class] ||= Hirb::Helpers::Table
+    if output.size == 1 && !options[:ask]
+      yield(output) if output.size > 0 and block_given?
+      return output
+    end
     $stdout.puts options[:helper_class].render(output, options.merge(:number=>true))
     $stdout.print options[:prompt]
     input = $stdin.gets.chomp.strip
@@ -14,6 +18,7 @@ class Hirb::Helpers::Menu
         return results[0]
       end
     end
+    yield(results) if results.size > 0 and block_given?
     results
   end
 end
