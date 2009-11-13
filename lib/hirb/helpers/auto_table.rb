7a6552985269436204dd7c32912559061b598b0d
Gabriel Horner <gabriel.horner@gmail.com>
2009-11-13 17:51:52 -0500

    changed deprecated to_a to Array

--- lib/hirb/helpers/auto_table.rb
+++ lib/hirb/helpers/auto_table.rb
@@ -2,10 +2,10 @@
 class Hirb::Helpers::AutoTable
   # Same options as Hirb::Helpers::Table.render.
   def self.render(output, options={})
-    output = output.to_a if !output.is_a?(Array) && output.respond_to?(:to_a)
-    klass = if ((output.is_a?(Array) && output[0].is_a?(ActiveRecord::Base)) or output.is_a?(ActiveRecord::Base) rescue false)
+    output = Array(output)
+    klass = if (output[0].is_a?(ActiveRecord::Base) rescue false)
       Hirb::Helpers::ActiveRecordTable
-    elsif (output.is_a?(Array) && !(output[0].is_a?(Hash) || output[0].is_a?(Array)))
+    elsif !(output[0].is_a?(Hash) || output[0].is_a?(Array))
       Hirb::Helpers::ObjectTable
     else
       Hirb::Helpers::Table
