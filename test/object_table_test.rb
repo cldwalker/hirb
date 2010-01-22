b1bf7e31bc2357b161f3b31b74c8c79f70832213
Gabriel Horner <gabriel.horner@gmail.com>
2010-01-22 06:02:39 -0500

    allow custom header for simple array tables

--- test/object_table_test.rb
+++ test/object_table_test.rb
@@ -2,6 +2,10 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 
 class Hirb::Helpers::ObjectTableTest < Test::Unit::TestCase
   context "object table" do
+    def table(*args)
+      Hirb::Helpers::ObjectTable.render(*args)
+    end
+
     before(:all) {
       @pets = [stub(:name=>'rufus', :age=>7, :to_s=>'rufus'), stub(:name=>'alf', :age=>101, :to_s=>'alf')]
     }
@@ -15,7 +19,7 @@ class Hirb::Helpers::ObjectTableTest < Test::Unit::TestCase
       +-------+-----+
       2 rows in set
       TABLE
-      Hirb::Helpers::ObjectTable.render(@pets, :fields=>[:name, :age]).should == expected_table
+      table(@pets, :fields=>[:name, :age]).should == expected_table
     end
     
     test "with no options defaults to to_s field" do
@@ -28,7 +32,7 @@ class Hirb::Helpers::ObjectTableTest < Test::Unit::TestCase
       +-------+
       2 rows in set
       TABLE
-      Hirb::Helpers::ObjectTable.render(@pets).should == expected_table
+      table(@pets).should == expected_table
     end
 
     test "renders simple arrays" do
@@ -43,14 +47,29 @@ class Hirb::Helpers::ObjectTableTest < Test::Unit::TestCase
       +-------+
       4 rows in set
       TABLE
-      Hirb::Helpers::ObjectTable.render([1,2,3,4]).should == expected_table
+      table([1,2,3,4]).should == expected_table
+    end
+
+    test "renders simple arrays with custom header" do
+      expected_table = <<-TABLE.unindent
+      +-----+
+      | num |
+      +-----+
+      | 1   |
+      | 2   |
+      | 3   |
+      | 4   |
+      +-----+
+      4 rows in set
+      TABLE
+      table([1,2,3,4], :headers=>{:to_s=>'num'}).should == expected_table
     end
 
     test "with empty fields" do
       expected_table = <<-TABLE.unindent
       0 rows in set
       TABLE
-      Hirb::Helpers::ObjectTable.render(@pets, :fields => []).should == expected_table
+      table(@pets, :fields => []).should == expected_table
     end
   end
 end
