4f48aa2015ebe35f1c314d7f1815bbd218ea534a
Gabriel Horner <gabriel.horner@gmail.com>
2010-01-27 13:38:25 -0500

    table: refactor common injects + remove stupid raise

--- lib/hirb/helpers/table.rb
+++ lib/hirb/helpers/table.rb
@@ -225,40 +225,40 @@ module Hirb
     max_width -= @fields.size * BORDER_LENGTH + 1
     original_field_lengths = field_lengths.dup
     @min_field_length = BORDER_LENGTH
-    adjust_long_fields(field_lengths, max_width)
-  rescue TooManyFieldsForWidthError
-    raise
-  rescue
-    default_restrict_field_lengths(field_lengths, original_field_lengths, max_width)
+    adjust_long_fields(field_lengths, max_width) ||
+      default_restrict_field_lengths(field_lengths, original_field_lengths, max_width)
+  end
+
+  def sum(arr)
+    arr.inject {|t,e| t += e }
   end
 
   # Simple algorithm which given a max width, allows smaller fields to be displayed while
   # restricting longer fields at an average_long_field_length.
   def adjust_long_fields(field_lengths, max_width)
-    total_length = field_lengths.values.inject {|t,n| t += n}
+    total_length = sum field_lengths.values
     while total_length > max_width
       raise TooManyFieldsForWidthError if @fields.size > max_width.to_f / @min_field_length
       average_field_length = total_length / @fields.size.to_f
       long_lengths = field_lengths.values.select {|e| e > average_field_length}
-      if long_lengths.empty?
-        raise "Algorithm didn't work, resort to default"
-      else
-        total_long_field_length = (long_lengths.inject {|t,n| t += n}) * max_width/total_length
-        average_long_field_length = total_long_field_length / long_lengths.size
-        field_lengths.each {|f,length|
-          field_lengths[f] = average_long_field_length if length > average_long_field_length
-        }
-      end
-      total_length = field_lengths.values.inject {|t,n| t += n}
+      return false if long_lengths.empty?
+
+      total_long_field_length = sum(long_lengths) * max_width/total_length
+      average_long_field_length = total_long_field_length / long_lengths.size
+      field_lengths.each {|f,length|
+        field_lengths[f] = average_long_field_length if length > average_long_field_length
+      }
+      total_length = sum field_lengths.values
     end
+    true
   end
 
   # Produces a field_lengths which meets the max_width requirement
   def default_restrict_field_lengths(field_lengths, original_field_lengths, max_width)
-    original_total_length = original_field_lengths.values.inject {|t,n| t += n}
+    original_total_length = sum original_field_lengths.values
     relative_lengths = original_field_lengths.values.map {|v| (v / original_total_length.to_f * max_width).to_i  }
     # set fields by their relative weight to original length
-    if relative_lengths.all? {|e| e > @min_field_length} && (relative_lengths.inject {|a,e| a += e} <= max_width)
+    if relative_lengths.all? {|e| e > @min_field_length} && (sum(relative_lengths) <= max_width)
       original_field_lengths.each {|k,v| field_lengths[k] = (v / original_total_length.to_f * max_width).to_i }
     else
       # set all fields the same if nothing else works
