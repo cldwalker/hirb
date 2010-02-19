2a5e7ac5866a91ef35f58660a41a1df26114d903
Gabriel Horner <gabriel.horner@gmail.com>
2010-02-19 02:11:27 -0500

    update import_object

--- lib/hirb/import_object.rb
+++ lib/hirb/import_object.rb
@@ -2,7 +2,7 @@ module Hirb
   module ObjectMethods
     # Takes same options as Hirb::View.render_output.
     def view(*args)
-      Hirb::View.console_render_output(*(args.unshift(self)))
+      Hirb::Console.render_output(*(args.unshift(self)))
     end
   end
 end
