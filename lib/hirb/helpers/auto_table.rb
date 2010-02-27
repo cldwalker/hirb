10b5fa060b5e4e9060f781d780985177ccd45e6e
Gabriel Horner <gabriel.horner@gmail.com>
2010-02-27 11:59:37 -0500

    first attempt at a more extendable auto_table

--- lib/hirb/helpers/auto_table.rb
+++ lib/hirb/helpers/auto_table.rb
@@ -7,10 +7,34 @@ class Hirb::Helpers::AutoTable
     klass = if (output[0].is_a?(ActiveRecord::Base) rescue false)
       Hirb::Helpers::ActiveRecordTable
     elsif !(output[0].is_a?(Hash) || output[0].is_a?(Array))
+      options = (object_table_options(output[0]) || {}).merge options
       Hirb::Helpers::ObjectTable
     else
       Hirb::Helpers::Table
     end
     klass.render(output, options)
+  end
+
+  def self.object_table_options(obj)
+    auto_table = new
+    auto_table.option_method_classes.each do |meth, klass|
+      if obj.class.ancestors.include?(Hirb::Util.any_const_get(klass))
+        return auto_table.send("#{meth}_options", obj) rescue {}
+      end
+    end
+  end
+
+  def option_method_classes
+    @option_method_classes ||= option_methods.inject({}) {|t,e|
+      t[e] = Hirb::Util.camelize e.gsub('__', '/') ; t
+    }
+  end
+
+  def option_methods
+    self.class.instance_methods(false).select {|e| e.to_s =~ /_options$/ }.map {|e| e.to_s.sub(/_options$/, '') }
+  end
+
+  def mongoid__document_options(obj)
+    {:fields=>obj.class.fields.keys}
   end
 end
