6415404a026888fbc41c543a1c87b2e853691884
Gabriel Horner <gabriel.horner@gmail.com>
2010-03-01 12:01:59 -0500

    sequel support

--- lib/hirb/helpers/auto_table/orm.rb
+++ lib/hirb/helpers/auto_table/orm.rb
@@ -3,6 +3,10 @@ class Hirb::Helpers::AutoTable
     def data_mapper__resource_options(obj)
       { :fields=>obj.class.properties.map {|e| e.name } }
     end
+
+    def sequel__model_options(obj)
+      {:fields=>obj.class.columns}
+    end
   end
 
   add_module ORM
