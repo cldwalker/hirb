57d8adae77ee5fecdcbbf6db97885a11e0ec1d91
Gabriel Horner <gabriel.horner@gmail.com>
2010-03-02 16:32:08 -0500

    test overriding a view

--- test/helper_view_test.rb
+++ test/helper_view_test.rb
@@ -3,7 +3,8 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 module Hirb
   class HelperViewTest < Test::Unit::TestCase
     context "helper_view" do
-      before(:all) {
+      before(:all) { View.load_config }
+      before(:each) {
         class ::Hirb::Helpers::AutoTable
           module Blah
             def date_options(obj)
@@ -13,13 +14,28 @@ module Hirb
           add_module Blah
         end
       }
+      after(:each) { Hirb::Helpers::AutoTable.send(:remove_const, :Blah) }
+      after(:all) { reset_config}
+
+      def output_expects(output, expects)
+        Helpers::ObjectTable.expects(:render).with(output, expects)
+        Helpers::AutoTable.render(output)
+      end
 
       test "sets options" do
-        View.load_config
-        output = [Date.new]
-        Helpers::ObjectTable.expects(:render).with(output, :fields=>Date::DAYNAMES)
-        Helpers::AutoTable.render(output)
-        reset_config
+        output_expects [Date.new], :fields=>Date::DAYNAMES
+      end
+
+      test "overrides a view's options" do
+        class ::Hirb::Helpers::AutoTable
+          module Blah2
+            def date_options(obj)
+              {:fields=>obj.class::DAYNAMES + ['blah']}
+            end
+          end
+          add_module Blah2
+        end
+        output_expects [Date.new], :fields=>(Date::DAYNAMES + ['blah'])
       end
     end
   end
