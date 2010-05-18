939ef9b32e385adfc972c6d4fdebec5aeb70752f
Gabriel Horner <gabriel.horner@gmail.com>
2010-05-18 17:39:21 -0400

    added completions for console methods

--- lib/bond/completions/hirb.rb
+++ lib/bond/completions/hirb.rb
@@ -1,7 +1,7 @@
 complete(:methods=>%w{Hirb::View.enable Hirb.enable}) {
   %w{config_file output_method output width height formatter pager pager_command}
 }
-complete(:method=>'Hirb::Helpers::Table.render') {
+complete(:methods=>%w{Hirb::Helpers::Table.render table}) {
   %w{fields headers max_fields max_width resize number change_fields}+
   %w{filters header_filter filter_any filter_classes vertical all_fields}+
   %w{description escape_special_chars table_class hide_empty}
@@ -9,6 +9,6 @@ complete(:method=>'Hirb::Helpers::Table.render') {
 complete(:method=>"Hirb::Helpers::Tree.render") {
   %w{type validate indent limit description multi_line_nodes value_method children_method}
 }
-complete(:method=>"Hirb::Menu.render") {
+complete(:methods=>%w{Hirb::Menu.render menu}) {
   %w{helper_class prompt ask directions readline two_d default_field action multi_action action_object command}
 }
