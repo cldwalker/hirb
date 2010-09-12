42a5f5a5f372f4e7d74a0865e611fbec7779d201
Gabriel Horner <gabriel.horner@gmail.com>
2010-09-12 13:54:23 -0400

    Don't use tput when hirb is running within cron

--- lib/hirb/util.rb
+++ lib/hirb/util.rb
@@ -61,10 +61,12 @@ module Hirb
     def detect_terminal_size
       if (ENV['COLUMNS'] =~ /^\d+$/) && (ENV['LINES'] =~ /^\d+$/)
         [ENV['COLUMNS'].to_i, ENV['LINES'].to_i]
-      elsif (RUBY_PLATFORM =~ /java/ || !STDIN.tty?) && command_exists?('tput')
+      elsif (RUBY_PLATFORM =~ /java/ || (!STDIN.tty? && ENV['TERM'])) && command_exists?('tput')
         [`tput cols`.to_i, `tput lines`.to_i]
+      elsif STDIN.tty? && command_exists?('stty')
+        `stty size`.scan(/\d+/).map { |s| s.to_i }.reverse
       else
-        command_exists?('stty') ? `stty size`.scan(/\d+/).map { |s| s.to_i }.reverse : nil
+        nil
       end
     rescue
       nil
