5cf1e2e6316e0bd7d5664d509e07ba54e96e43be
Jan Lelis <mail@janlelis.de>
2010-12-09 01:42:49 +0100

    fixed bug when requireing ripl/hirb within a ripl session

--- lib/ripl/hirb.rb
+++ lib/ripl/hirb.rb
@@ -1,9 +1,15 @@
 require 'hirb'
 
 module Ripl::Hirb
+  class << self
+    def enable
+      Hirb.enable(Ripl.config[:hirb] || {}) unless Hirb::View.enabled?
+    end
+  end
+
   def before_loop
     super
-    Hirb.enable(Ripl.config[:hirb] || {}) unless Hirb::View.enabled?
+    Ripl::Hirb.enable
   end
 
   def format_result(result)
@@ -13,3 +19,4 @@ module Ripl::Hirb
 end
 
 Ripl::Shell.send :include, Ripl::Hirb
+Ripl::Hirb.enable if Ripl.instance_variable_get(:@shell) # TODO non hacky way to detect if in ripl session or not
