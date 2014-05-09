bc5e6cfce85ce883d74f6ca9977bb4bf45b1f1a8
Fission Xuiptz <fissionxuiptz@softwaremojo.com>
2014-05-09 14:02:28 +1000

    Fix Hirb breaking on AR tables with no id column
    Hirb kills a puppy every time you give it an ActiveRecord::Base table with no id column. I've heard that when a puppy dies it cries out 'comparison of String with nil failed'! This patch will save many puppies by compacting an object's attribute keys before comparing it with the object's class's column_names.

--- lib/hirb/views/rails.rb
+++ lib/hirb/views/rails.rb
@@ -6,8 +6,8 @@ module Hirb::Views::Rails #:nodoc:
   def get_active_record_fields(obj)
     fields = obj.class.column_names.map {|e| e.to_sym }
     # if query used select
-    if obj.attributes.keys.sort != obj.class.column_names.sort
-      selected_columns = obj.attributes.keys
+    if obj.attributes.keys.compact.sort != obj.class.column_names.sort
+      selected_columns = obj.attributes.keys.compact
       sorted_columns = obj.class.column_names.dup.delete_if {|e| !selected_columns.include?(e) }
       sorted_columns += (selected_columns - sorted_columns)
       fields = sorted_columns.map {|e| e.to_sym}
